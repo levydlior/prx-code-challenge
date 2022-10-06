0f99b5b3c21d4f8b86b3a89029939212486c29dc
Lior <levydlior@gmail.com>
2022-10-06 14:36:39 -0700

    refractored App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,12 @@
-import "./App.css";
-import React, { useEffect, useState } from "react";
-import { ActionCableConsumer } from "react-actioncable-provider";
+import React from 'react'
+import MainContent from './components/MainContent'
 
 function App() {
-  const [blogs, setBlogs] = useState([[]]);
-
-  useEffect(() => {
-    fetch("/blogs")
-      .then((r) => r.json())
-      .then((parsedBlogsArray) => setBlogs(parsedBlogsArray));
-  }, []);
-
-  console.log(blogs);
-
-  const mapsArray = blogs[0].map((blog) => {
-    function wordCountList() {
-      return Object.entries(blog.word_count).map(([key, value], i) => {
-        return (
-          <li key={key}>
-            {key}: {value}
-          </li>
-        );
-      });
-    }
-
-    return (
-      <div key={blog.title}>
-        <h2>{blog.title}</h2>
-        <p>{blog.content}</p>
-        <h3>Word Count:</h3>
-        <ul>{wordCountList()}</ul>
-      </div>
-    );
-  });
-
-  function handleReceived(newBlogs) {
-    console.log(newBlogs);
-    setBlogs(newBlogs);
-  }
-
   return (
-    <div className="App">
-      <ActionCableConsumer
-        channel={{ channel: "BlogsChannel" }}
-        onReceived={handleReceived}
-      />
-      {mapsArray}
+    <div>
+      <MainContent />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -1,22 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
+import { ActionCableConsumer } from "react-actioncable-provider";
 
 function MainContent() {
+  const [blogs, setBlogs] = useState([[]]);
 
   useEffect(() => {
-    fetch('/blogs')
-    .then(r => {
-      if (r.ok){
-        r.json().then(parsedR => console.log(parsedR))
-      }else {
-        r.json().then(err => console.log(err))
-      }
-    })
-  })
+    fetch("/blogs")
+      .then((r) => r.json())
+      .then((parsedBlogsArray) => setBlogs(parsedBlogsArray));
+  }, []);
+
+  console.log(blogs);
+
+  const mapsArray = blogs[0].map((blog) => {
+    function wordCountList() {
+      return Object.entries(blog.word_count).map(([key, value], i) => {
+        return (
+          <li key={key}>
+            {key}: {value}
+          </li>
+        );
+      });
+    }
+
+    return (
+      <div key={blog.title}>
+        <h2>{blog.title}</h2>
+        <p>{blog.content}</p>
+        <h3>Word Count:</h3>
+        <ul>{wordCountList()}</ul>
+      </div>
+    );
+  });
+
+  function handleReceived(newBlogs) {
+    console.log(newBlogs);
+    setBlogs(newBlogs);
+  }
+
   return (
-    <div>
-    <p> hi</p>
+    <div >
+      <ActionCableConsumer
+        channel={{ channel: "BlogsChannel" }}
+        onReceived={handleReceived}
+      />
+      {mapsArray}
     </div>
-  )
+  );
 }
 
-export default MainContent
+export default MainContent;
