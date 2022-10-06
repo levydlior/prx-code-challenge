8c00922ba5504feaa9738ff9d749ec12f1f17d64
Lior <levydlior@gmail.com>
2022-10-06 15:06:09 -0700

    add style:

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -1,11 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const BlogCardDiv = styled.div`
 border: solid 1px;
 margin: 0.5rem;
 padding: 0.5rem;
 `
+
+const TitleOne = styled.h2`
+padding 1rem;
+`
+const TitleTwo = styled.h3`
+padding 1rem;
+`
+
+
 
 const Ul = styled.ul`
 display: flex;
@@ -31,19 +46,24 @@ function BlogCard({blog}) {
         return Object.entries(blog.word_count).map(([key, value], i) => {
           return (
             <Li key={key}>
-              {key}:{value}
+              {key}: {value}
             </Li>
           );
         });
       }
 
   return (
-    <BlogCardDiv key={blog.title}>
-      <h2>{blog.title}</h2>
+
+<Card sx={{ minWidth: 275, margin: "1rem" }}>
+<CardContent>
+<TitleOne>{blog.title}</TitleOne>
       <P>{blog.content}</P>
-      <h3>Word Count:</h3>
+      <TitleTwo>Word Count:</TitleTwo>
       <Ul>{wordCountList()}</Ul>
-    </BlogCardDiv>
+</CardContent>
+</Card>
+
+
   );
 }
 
