33f512c150819cf523b377262c5842591d12a384
Lior <levydlior@gmail.com>
2022-10-06 11:25:52 -0700

    working on actioncable

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
-import MainContent from './components/MainContent'
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { ActionCableConsumer } from "react-actioncable-provider";
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("/blogs")
+      .then((r) => r.json())
+      .then((parsedBlogsArray) => setBlogs(parsedBlogsArray));
+  }, []);
+
+  function fetchBlogs() {
+    fetch("/blogs")
+      .then((r) => r.json())
+      .then((parsedBlogsArray) => setBlogs(parsedBlogsArray));
+  }
+
+
+  console.log(blogs)
+  function handleReceived(newBlogs) {
+    console.log(newBlogs);
+    setBlogs(newBlogs)
+  }
+
   return (
     <div className="App">
-    <MainContent />
+      <ActionCableConsumer
+        channel={{ channel: "BlogsChannel" }}
+        onReceived={handleReceived}
+      />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {ActionCableProvider} from 'react-actioncable-provider'
+import actionCable from 'actioncable';
+
+const CableApp = {}
+
+CableApp.cable = actionCable.createConsumer(`ws://localhost:3000/cable`)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ActionCableProvider url={`ws://localhost:3000/cable`}>
     <App />
-  </React.StrictMode>
+    </ActionCableProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
